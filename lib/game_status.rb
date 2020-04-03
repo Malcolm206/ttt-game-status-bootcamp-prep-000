d72a658e0fff484dd0db1e81abd6137853251707
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2020-04-03 21:06:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,11 +39,9 @@ end
 
 def winner(board)
   winning = won?(board)
-  if board[winning[0]] == "X"
+  if winning == false
+    puts nil
+  elsif board[winning[0]] == "X"
     puts "X"
   elsif board[winning[0]] == "O"
     puts "O"
-  else
-    puts nil
-  end
-end
